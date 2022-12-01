6dbcb1d916f5a91eecd8e5d1590a9047a3268196
Shwetank Kumar <shwetank.kumar@gmail.com>
2022-12-01 11:45:00 -0800

    spec add username

--- optiml/queries.py
+++ optiml/queries.py
@@ -390,6 +390,7 @@ class SNFLKQuery():
         return self.query_to_df(sql)
 
     # @simple_cache
+    ##TODO: Make this return user name
     def cost_of_compute_ts(self, start_date='2022-01-01', end_date=''):
         """Calculates the cost of compute in time series for a given time period using Snowflake Warehouse
         Metering History tables. Outputs a dataframe with the following columns:
@@ -606,4 +607,69 @@ class SNFLKQuery():
         df.insert(1, "end_time", df["start_time"] + pd.offsets.Hour(1))
         return df
 
-    
+    def cost_by_partner_tool_ts(self, start_date='2022-01-01', end_date=''):
+        ##TODO: Convert partner tool consumption query into 
+        # a timeseries so that its consistent with others and can be plotted out
+        """
+        Calculates the overall cost of usage by partner application over a
+        Outputs a dataframe with the following columns:
+        Category name: Category name as Storage
+        Usage date: The date on which storage is used
+        Dollars used: Total cost of storage (in dollars) used
+        """
+        if not end_date:
+            today_date = date.today()
+            end_date = str(today_date)
+        sql = f"""-- THIS IS APPROXIMATE CREDIT CONSUMPTION BY CLIENT APPLICATION
+            with client_hour_execution_cte as (
+                select  case
+                    when client_application_id like 'Go %' then 'Go'
+                    when client_application_id like 'Snowflake UI %' then 'Snowflake UI'
+                    when client_application_id like 'SnowSQL %' then 'SnowSQL'
+                    when client_application_id like 'JDBC %' then 'JDBC'
+                    when client_application_id like 'PythonConnector %' then 'Python'
+                    when client_application_id like 'ODBC %' then 'ODBC'
+                    else 'NOT YET MAPPED: ' || client_application_id
+                    end as client_application_name
+                ,warehouse_name
+                ,date_trunc('hour',start_time) as start_time_hour
+                ,sum(execution_time)  as client_hour_execution_time
+                from {self.dbname}.ACCOUNT_USAGE.QUERY_HISTORY qh
+                join {self.dbname}.ACCOUNT_USAGE.SESSIONS se on se.session_id = qh.session_id
+                where warehouse_name is not null
+                    and execution_time > 0
+
+            -- Change the below filter if you want to look at a longer range than the last 1 month
+                    and start_time between '{start_date}' and '{end_date}'
+                group by 1,2,3
+                )
+            , hour_execution_cte as (
+                select start_time_hour
+                    ,warehouse_name
+                    ,sum(client_hour_execution_time) as hour_execution_time
+                from client_hour_execution_cte
+                group by 1,2
+            )
+            , approximate_credits as (
+                select
+                    a.client_application_name
+                    ,c.warehouse_name
+                    ,(a.client_hour_execution_time/b.hour_execution_time)*c.credits_used as approximate_credits_used
+
+                from client_hour_execution_cte a
+                join hour_execution_cte b  on a.start_time_hour = b.start_time_hour and b.warehouse_name = a.warehouse_name
+                join {self.dbname}.ACCOUNT_USAGE.WAREHOUSE_METERING_HISTORY c on c.warehouse_name = a.warehouse_name and c.start_time = a.start_time_hour
+            )
+
+            select
+                client_application_name
+                ,warehouse_name
+                ,sum(approximate_credits_used) as approximate_credits_used
+            from approximate_credits
+            group by 1,2
+            order by 3 desc
+            ;"""
+
+        
+        df = self.query_to_df(sql)
+        return df
