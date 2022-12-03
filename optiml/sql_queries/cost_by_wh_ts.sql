13bdd0384ea3d95064bdf60e763200a5d3e6883e
Shwetank Kumar <shwetank.kumar@gmail.com>
2022-12-03 07:18:47 -0800

    removing sql_queries folder

--- optiml/sql_queries/cost_by_wh_ts.sql
+++
@@ -1,8 +0,0 @@
-select warehouse_name
-      ,credits_used
-      ,start_time
-      ,end_time
-from account_usage.warehouse_metering_history
-where start_time between '2022-05-01' and '2022-05-31' -->= dateadd(day, -5, current_timestamp())  -- Past m days
-group by 1,2,3,4
-order by 3 asc;
--- optiml/sql_queries/cost_of_snowpipe_ts.sql
+++
@@ -1,8 +0,0 @@
-select
-  pipe_name
-  ,credits_used
-  ,start_time
-  ,end_time
-from "KIV"."ACCOUNT_USAGE"."PIPE_USAGE_HISTORY"
-where start_time between '2022-05-01' and '2022-05-31'
-order by 1 desc;
