ff13d74eed4d55a72248a3a6c7d5e84f36bda607
nikhil-teqfocus <nikhil@teqfocus.com>
2022-12-06 13:47:22 +0530

    Updated query to calculate n expensive queries

--- optiml/queries.py
+++ optiml/queries.py
@@ -745,82 +745,27 @@ class SNFLKQuery():
 
 
 ## Query cost related queries
-##TODO: 1) Check query 2) Add flag to give unique query text with parameters 
-    def n_expensive_queries(self, start_date='2022-01-01', end_date='', n=10):
+##TODO: 1) Check query 2) Add flag to give unique query text with parameters
+    def n_expensive_queries(self, n=10):
         """
-        Calculates expense of queries over a specific time period
+        Calculates n expensive queries
         """
-        if not end_date:
-            today_date = date.today()
-            end_date = str(today_date)
-        credit_val = ''
-        if self.credit_value:
-            credit_val = SNFLKQuery.credit_values[self.credit_value]
+        # if not end_date:
+        #     today_date = date.today()
+        #     end_date = str(today_date)
+        # credit_val = ''
+        # if self.credit_value:
+        #     credit_val = SNFLKQuery.credit_values[self.credit_value]
         sql = f"""
-        WITH WAREHOUSE_SIZE AS
-        (
-             SELECT WAREHOUSE_SIZE, NODES
-               FROM (
-                      SELECT 'XSMALL' AS WAREHOUSE_SIZE, 1 AS NODES
-                      UNION ALL
-                      SELECT 'SMALL' AS WAREHOUSE_SIZE, 2 AS NODES
-                      UNION ALL
-                      SELECT 'MEDIUM' AS WAREHOUSE_SIZE, 4 AS NODES
-                      UNION ALL
-                      SELECT 'LARGE' AS WAREHOUSE_SIZE, 8 AS NODES
-                      UNION ALL
-                      SELECT 'XLARGE' AS WAREHOUSE_SIZE, 16 AS NODES
-                      UNION ALL
-                      SELECT '2XLARGE' AS WAREHOUSE_SIZE, 32 AS NODES
-                      UNION ALL
-                      SELECT '3XLARGE' AS WAREHOUSE_SIZE, 64 AS NODES
-                      UNION ALL
-                      SELECT '4XLARGE' AS WAREHOUSE_SIZE, 128 AS NODES
-                    )
-        ),
-        QUERY_HISTORY AS
-        (
-             SELECT QH.QUERY_ID
-                   ,QH.QUERY_TEXT
-                   ,QH.USER_NAME
-                   ,QH.ROLE_NAME
-                   ,QH.EXECUTION_TIME
-                   ,QH.WAREHOUSE_SIZE
-              FROM {self.dbname}.ACCOUNT_USAGE.QUERY_HISTORY QH
-             WHERE START_TIME between '{start_date}' and '{end_date}'
-        )
-
-        SELECT QH.QUERY_ID
-              ,QH.QUERY_TEXT
-              ,QH.USER_NAME
-              ,QH.ROLE_NAME
-              ,(QH.EXECUTION_TIME/(1000*60)) AS EXECUTION_TIME_MINUTES
-              ,WS.WAREHOUSE_SIZE
-              ,WS.NODES
-              ,WC.CREDITS
-              ,(WC.DOLLARS/QC.QUERY_COUNT) as cost_per_query
-        FROM QUERY_HISTORY QH
-        JOIN WAREHOUSE_SIZE WS ON WS.WAREHOUSE_SIZE = upper(QH.WAREHOUSE_SIZE)
-        JOIN  (
-            SELECT
-               WAREHOUSE_NAME
-              ,COUNT(QUERY_ID) as QUERY_COUNT
-            FROM {self.dbname}.ACCOUNT_USAGE.QUERY_HISTORY
-            WHERE TO_DATE(START_TIME) between '{start_date}' and '{end_date}'
-            GROUP BY WAREHOUSE_NAME
-              ) QC
-        JOIN (
-            SELECT
-                WAREHOUSE_NAME
-                ,SUM(CREDITS_USED) as CREDITS
-                ,SUM(CREDITS_USED)*({credit_val}) as DOLLARS
-            FROM {self.dbname}.ACCOUNT_USAGE.WAREHOUSE_METERING_HISTORY
-            WHERE TO_DATE(START_TIME) between '{start_date}' and '{end_date}'
-            GROUP BY WAREHOUSE_NAME
-          ) WC
-        ORDER BY CREDITS DESC LIMIT {n}
+        SELECT DISTINCT
+           QH.QUERY_TEXT
+           ,QH.USER_NAME
+           ,QH.ROLE_NAME
+           --,SUM(QH.EXECUTION_TIME) AS TOTAL_EXECUTION_TIME
+           ,QH.WAREHOUSE_TYPE
+           ,SUM(QH.CREDITS_USED_CLOUD_SERVICES) as CREDITS
+        FROM {self.dbname}.ACCOUNT_USAGE.QUERY_HISTORY QH
+        group by QUERY_TEXT, USER_NAME, ROLE_NAME, WAREHOUSE_TYPE order by CREDITS desc limit {n}
         """
         df = self.query_to_df(sql)
         return df
-
-
