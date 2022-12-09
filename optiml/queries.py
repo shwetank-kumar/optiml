3c53c0256d3fcba8f03a435775f824689ecc30a4
Manasvini <manasvini.srini@gmail.com>
2022-12-09 09:58:53 -0800

    Table_accessed function

--- optiml/queries.py
+++ optiml/queries.py
@@ -948,8 +948,29 @@ class SNFLKQuery():
         """
         df=self.query_to_df(sql)
         return df
-     
+    # They will show up if they have been accessed
+    def table_accessed(self,start_date="2022-01-01", end_date="2022-02-02"):
+        if not end_date:
+            today_date = date.today()
+            end_date = str(today_date)
+        sql=f"""
+        SELECT query_id
+        , query_start_time
+        FROM {self.dbname}.ACCOUNT_USAGE.access_history
+        , lateral flatten(base_objects_accessed) f1
+        WHERE f1.value:"objectId"::int=32998411400350
+        AND f1.value:"objectDomain"::string='Table'
+        AND query_start_time >= dateadd('month', -6, current_timestamp())
+        """
+        df=self.query_to_df(sql)
+        return df
+
         
+        
+        
+    
+
+
         
    
     
