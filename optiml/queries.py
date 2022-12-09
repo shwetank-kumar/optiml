f4e19825ced144c921378fd812a29fdfaec2f128
Manasvini <manasvini.srini@gmail.com>
2022-12-08 20:06:51 -0800

    more functions added

--- optiml/queries.py
+++ optiml/queries.py
@@ -899,6 +899,59 @@ class SNFLKQuery():
         """
         df=self.query_to_df(sql)
         return df
+    def idle_roles(self,start_date="2022-01-01", end_date=""):
+        if not end_date:
+            today_date = date.today()
+            end_date = str(today_date)
+
+        sql=f"""
+        SELECT 
+        R.*
+        FROM SNOWFLAKE.ACCOUNT_USAGE.ROLES R
+        LEFT JOIN (
+            SELECT DISTINCT 
+                ROLE_NAME 
+            FROM {self.dbname}.ACCOUNT_USAGE.QUERY_HISTORY 
+            WHERE START_TIME > DATEADD(month,-1,CURRENT_TIMESTAMP())
+                ) Q 
+                        ON Q.ROLE_NAME = R.NAME
+        WHERE Q.ROLE_NAME IS NULL
+        and DELETED_ON IS NULL;
+        """
+        df=self.query_to_df(sql)
+        return df
+    def failed_tasks(self,start_date="2022-01-01", end_date=""):
+        if not end_date:
+            today_date = date.today()
+            end_date = str(today_date)
+        sql=f"""
+        select *
+        from {self.dbname}.account_usage.task_history
+        WHERE STATE = 'FAILED'
+        and query_start_time >= DATEADD (day,-7, CURRENT_TIMESTAMP())
+        order by query_start_time DESC
+        
+        """
+        df=self.query_to_df(sql)
+        return df
+    def long_running_tasks(self,start_date="2022-01-01", end_date=""):
+        if not end_date:
+            today_date = date.today()
+            end_date = str(today_date)
+        sql=f"""
+        select DATEDIFF(seconds, QUERY_START_TIME,COMPLETED_TIME) as DURATION_SECONDS
+        ,*
+        from {self.dbname}.account_usage.task_history
+        WHERE STATE = 'SUCCEEDED'
+        and query_start_time >= DATEADD (day, -7, CURRENT_TIMESTAMP())
+        order by DURATION_SECONDS desc
+        """
+        df=self.query_to_df(sql)
+        return df
+     
+        
+        
+   
     
 
   
