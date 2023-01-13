90a82695fb7549f94be24d3b9d354447238c740d
Manasvini <manasvini.srini@gmail.com>
2023-01-12 17:44:37 -0800

    App and dash for cost analysis

--- optiml/app.py
+++ optiml/app.py
@@ -18,12 +18,21 @@ color_scheme=["red","blue","green","orange","purple","brown","pink","gray","oliv
 df_total_usage_cost=com.usage_cost()
 fig1,fig2=com.cost_by_usage_plots()
 df_by_user,df_low_usage_users,df_by_user_consolidated,fig3,fig4=com.cost_by_user_plots()
+df_by_wh,fig_wh,fig_wh_ts=com.cost_by_warehouse_plots()
+df_by_pt,fig_pt,fig_pt_ts,fig_pt_wh=com.cost_by_pt()
+df_stats=com.stats()
+##DATAFRAMES
 
-table1=dash_table.DataTable(df_total_usage_cost.to_dict('records'),id="table")
-table_users=dash_table.DataTable(df_by_user.to_dict('records'),id="table")
-table_low_usage=dash_table.DataTable(df_low_usage_users.to_dict('records'),id="table")
-table_user_consolidated=dash_table.DataTable(df_by_user_consolidated.to_dict('records'),id="table")
+table_usage=dash_table.DataTable(df_total_usage_cost.to_dict('records'),id="table_usage")
+table_users=dash_table.DataTable(df_by_user.to_dict('records'),id="table_users")
+table_low_usage=dash_table.DataTable(df_low_usage_users.to_dict('records'),id="table_low_usage")
+table_user_consolidated=dash_table.DataTable(df_by_user_consolidated.to_dict('records'),id="table_user_consolidated")
+table_warehouse=dash_table.DataTable(df_by_wh.to_dict('records'),id="table_warehouse")
+table_pt=dash_table.DataTable(df_by_pt.to_dict('records'),id="table_pt")
+table_stats=dash_table.DataTable(df_stats.to_dict('records'),id="table_min_max")
+
 app = Dash(external_stylesheets=[dbc.themes.BOOTSTRAP])
+
 auth = dash_auth.BasicAuth(
     app,
     USERNAME_PASSWORD_PAIRS
@@ -81,8 +90,11 @@ app.layout = html.Div([dcc.Location(id="url"),html.H3(children='Kiva', style={
 def render_page_content(pathname):
 
     if pathname == "/":
-        return[html.H3("Cost by usage category"),table1,html.H3("Pie chart - cost by usage"),dcc.Graph(id="graph1", figure=fig1),html.H3("Cost by usage -Time series"),dcc.Graph(id="graph2", figure=fig2),
-        html.H3("Cost by user(Current month)"),table_users,html.H3("Low usage users"),table_low_usage,html.H3("Low usage users consolidated"),table_user_consolidated,html.H3("Pie chart -users"),dcc.Graph(id="graph3", figure=fig3),html.H3("Time series- users"),dcc.Graph(id="graph4", figure=fig4)
+        return[html.H3("Cost by usage category"),table_usage,html.H3("Statistics"),table_stats,html.H3("Pie chart - cost by usage"),dcc.Graph(id="graph1", figure=fig1),html.H3("Cost by usage -Time series"),dcc.Graph(id="graph2", figure=fig2),
+        html.H3("Cost by user(Current month)"),table_users,html.H3("Low usage users"),table_low_usage,html.H3("Low usage users consolidated"),table_user_consolidated,html.H3("Pie chart -users"),dcc.Graph(id="graph3", figure=fig3),html.H3("Time series- users"),dcc.Graph(id="graph4", figure=fig4),html.H3("Cost by warehouse"),
+        table_warehouse,html.H3("Cost by warehouse plot"),dcc.Graph(id="graph_wh_", figure=fig_wh),html.H3("Cost by warehouse - time series"),dcc.Graph(id="graph_ts", figure=fig_wh_ts),html.H3("Cost by partner tools"),table_pt,html.H3("Cost of partner tools -pie chart "),dcc.Graph(id="graph_pt", figure=fig_pt),html.H3("Cost of partner tools -time series"),dcc.Graph(id="graph_pt_ts", figure=fig_pt_ts),
+        html.H3("Cost by partner tools - warehouse"),dcc.Graph(id="graph_wh_", figure=fig_pt_wh[0]),dcc.Graph(id="graph_wh_", figure=fig_pt_wh[1]),dcc.Graph(id="graph_wh_", figure=fig_pt_wh[2]),dcc.Graph(id="graph_wh_", figure=fig_pt_wh[3]),
+
         ]
     elif pathname == "/page-1":
         return html.P("This is the content of page 1. Yay!")
