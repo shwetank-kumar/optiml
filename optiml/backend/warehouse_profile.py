b83854abff8eaef53e7705fdc469deb2fdfa3d97
Shwetank Kumar <shwetank.kumar@gmail.com>
2023-01-22 15:31:37 -0800

    wh_analysis

--- optiml/backend/warehouse_profile.py
+++ optiml/backend/warehouse_profile.py
@@ -11,6 +11,73 @@ class WarehouseProfile(SNFLKQuery):
         return df
 
     def wh_queued_load_ts(self,start_date="2022-01-01", end_date="",wh_name='DEV_WH',delta='minute'):
+        """
+        Shows the average queued load value in a given warehouse during a time interval (minute,hour,etc.)
+        Outputs a dataframe with the following columns:
+        Avg_running: Average running load of warehouse in given time interval.
+        Avg_queued_load: Average queued load of warehouse in given time interval.
+        query_count: Number of queries issued in warehouse in time interval.
+        """
+        if not end_date:
+            today_date = date.today()
+            end_date = str(today_date)
+        sql=f"""
+        WITH wlh as (
+        SELECT DATE_TRUNC('{delta}', wlh.start_time) hourly_start_time,
+        AVG(wlh.avg_running) as avg_running_load, AVG(wlh.avg_queued_load) as avg_queued_load 
+        FROM {self.dbname}.account_usage.warehouse_load_history wlh
+        WHERE DATE_TRUNC('DAY', wlh.start_time) between'{start_date}' and '{end_date}'
+        AND wlh.warehouse_name = '{wh_name}'
+        GROUP BY hourly_start_time
+        ORDER BY hourly_start_time asc
+        ),
+        qh as (
+        SELECT DATE_TRUNC('{delta}', qh.start_time) hourly_start_time,
+        COUNT(*) query_count
+        FROM {self.dbname}.account_usage.query_history qh
+        WHERE DATE_TRUNC('DAY', qh.start_time) between'{start_date}' and '{end_date}'
+        AND qh.warehouse_name = '{wh_name}'
+        GROUP BY  hourly_start_time
+        ORDER BY  hourly_start_time
+        )
+        SELECT wlh.hourly_start_time, 
+        wlh.avg_running_load, 
+        wlh.avg_queued_load, 
+        qh.query_count
+        FROM wlh,qh
+        WHERE wlh.hourly_start_time = qh.hourly_start_time
+        """
+        df=self.query_to_df(sql)
+        return df
+    
+    def wh_credits(self,start_date="2022-01-01", end_date="",wh_name='DEV_WH',delta='minute'):
+        if not end_date:
+            today_date = date.today()
+            end_date = str(today_date)
+        sql=f"""
+        WITH wlh as (
+        SELECT DATE_TRUNC('{delta}', wl.start_time) start_time_truncated,
+        AVG(avg_running) avg_running, AVG(avg_queued_load) avg_queued_load 
+        FROM {self.dbname}.account_usage.warehouse_load_history wl
+        WHERE DATE_TRUNC('DAY', wl.start_time) between'{start_date}' and '{end_date}'
+        AND wl.warehouse_name = '{wh_name}'
+        GROUP BY  start_time_truncated
+        ORDER BY start_time_truncated asc
+        ),
+        wmh AS (SELECT DATE_TRUNC('{delta}', wm.start_time) start_time_truncated, credits_used
+           FROM {self.dbname}.account_usage.warehouse_metering_history wm
+          WHERE DATE_TRUNC('DAY', wm.start_time) between '{start_date}' and '{end_date}'
+            AND wm.warehouse_name = '{wh_name}'
+            ORDER BY start_time_truncated ASC)
+        SELECT wlh.*, wmh.credits_used
+        FROM   wlh, wmh
+        WHERE  wlh.start_time_truncated = wmh.start_time_truncated
+                
+        """
+        df=self.query_to_df(sql)
+        return df
+    
+    def wh_metrics(self,start_date="2022-01-01", end_date="",wh_name='DEV_WH',delta='minute'):
         if not end_date:
             today_date = date.today()
             end_date = str(today_date)
