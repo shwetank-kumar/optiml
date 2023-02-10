14bafa90cdac7e89b4b66e335916b1164ad08bdd
giri-ggr <nawab.hayat@h1insights.com>
2023-02-10 21:12:28 +0530

    Script updated

--- scripts/snowflake_to_gcs.py
+++ scripts/snowflake_to_gcs.py
@@ -1,25 +1,30 @@
+import os.path
 import pickle
 import json
 import snowflake.connector
 import logging
 import datetime
 from pandas import read_sql, DataFrame
+from credentials import *
+
 
 # import pendulum
-
-# ToDo: 1. Get data between two timestamp and save it to pickle or local csv format.
 
 # from src.etl.lib.s3_utils import AnbS3BucketUtils, get_s3_bucket_credentials
 # from src.etl.lib.log_utils import get_logger
 
 # logger = get_logger(__name__)
 
-user = "SARWAR"
-password = "Reshu@123"
-warehouse = "XSMALL_WH"
-accountname = "VUJSGPR-XG91870"
-role = "ACCOUNTADMIN"
-database = 'SNOWFLAKE'
+
+def if_path_exist(func):
+    def wrapper_func(data, filename, *args, **kwargs):
+        directory = os.path.dirname(filename)
+        if not os.path.exists(directory):
+            os.makedirs(directory)
+        func(data, filename, *args, **kwargs)
+        # Do something after the function.
+
+    return wrapper_func
 
 
 def read_params(param_path):
@@ -33,17 +38,27 @@ def get_query(params, **inputs):
     table = inputs['table'].lower()
     print(f"Getting query for table :: {table}")
     if table not in params:
-        raise Exception("Input table is not available")
+        # raise Exception("Input table is not available")
+        print("Input table is not available.")
+        return False
 
     start_time = datetime.datetime.strptime(inputs['end_time'], "%Y-%m-%d") - datetime.timedelta(
         days=inputs['timedelta'])
 
+    if not inputs['timestamp_col']:
+        # raise Exception(f"No timestamp col for table :: {inputs['table']}")
+        print(f"No timestamp col for table :: {inputs['table']}")
     # Case 1: When we have start and end time.
-    if "START_TIME" in params[table] and "END_TIME" in params[table]:
+    if "START_TIME" == params[table]['timestamp_col']:
         sql = f"""select * from {inputs["database"]}.{inputs['schema']}.{inputs['table']} where start_time >= '{start_time}' and end_time <= '{inputs['end_time']}'
                 """
-    # Case  2: Without start & end time
-    # ToDo
+    # Case  2: fetch full data.
+    elif "full_table" == params[table]['timestamp_col']:
+        sql = f"""select * from {inputs["database"]}.{inputs['schema']}.{inputs['table']}"""
+
+    else:
+        # Case  3: Without start & end time
+        sql = f"""select * from {inputs["database"]}.{inputs['schema']}.{inputs['table']} where {inputs['timestamp_col']} >= '{start_time}' """
     print("Input Query :: ", sql)
     return sql
 
@@ -74,6 +89,7 @@ def get_views(schema):
     print(df)
 
 
+@if_path_exist
 def pickle_it(data, filename):
     pickle.dump(data, open(filename, "wb"))
     print(f"Pickled data saved to :: {filename}")
@@ -91,24 +107,36 @@ def get_table_data(sql):
     return df
 
 
-inputs = dict(
-    param_path='params.json',
-    database="SNOWFLAKE",
-    schema='ACCOUNT_USAGE',
-    table='QUERY_HISTORY',
-    timedelta=7,
-    end_time='2022-11-08',  # format: YYYY-MM-DD
-    path_to_pickle="query_history_2022-11-08",
-    do_pickle=True)
-
-if __name__ == '__main__':
+def main(params, **input):
     # Read Params
-    params = read_params(param_path=inputs['param_path'])
+    # params = read_params(param_path=inputs['param_path'])
     # Create Query
     query = get_query(params, **inputs)
     # Create Snowflake Connection
     conn = get_sf_conn()
     # Get data from table
-    df = get_table_data(query)
-    if inputs['do_pickle']:
-        pickle_it(data=df, filename=inputs['path_to_pickle'])
+    if query:
+        df = get_table_data(query)
+        if df.shape[0] and inputs['do_pickle']:
+            pickle_it(data=df, filename=inputs['path_to_pickle'])
+
+
+if __name__ == '__main__':
+    inputs = dict(
+        param_path='newparams.json',
+        database="SNOWFLAKE",
+        schema='ACCOUNT_USAGE',
+        table='QUERY_HISTORY',
+        timedelta=7,
+        end_time='2022-11-08',  # format: YYYY-MM-DD
+        path_to_pickle="query_history_2022-11-08",
+        timestamp_col="",
+        do_pickle=True)
+    params = read_params(param_path=inputs['param_path'])
+    for table, values in params.items():
+        inputs["path_to_pickle"] = os.path.join("pickle_data", inputs['table'],
+                                                f"{inputs['table']}_{inputs['end_time']}")
+        inputs['table'] = table
+        inputs['timestamp_col'] = values['timestamp_col']
+        main(params, **inputs)
+        print(f"Completed for {table}\n---------")
