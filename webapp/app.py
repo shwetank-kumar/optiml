ece1778c407e3cf4b69db28f459ddbdcd242f879
giri-ggr <nawab.hayat@h1insights.com>
2023-02-23 18:55:25 +0530

    Added config

--- webapp/app.py
+++ webapp/app.py
@@ -1,26 +1,21 @@
 import pathlib, sys
 
 dir_path = str(pathlib.Path.cwd().parent)
-print("Dirpath:  ", dir_path)
 sys.path.append(dir_path)
+from config import set_params
+set_params()
 import pandas as pd
 from homepage import Homepage
 from dashboard import show_dashboard
 import streamlit as st
 import os
 from streamlit_option_menu import option_menu
-import sys, pathlib, os
 from optiml.backend.cost_profile import CostProfile, get_previous_dates
 from optiml.connection import SnowflakeConnConfig
-pd.set_option('precision', 2)
-pd.set_option('float_format', '{:.2f}'.format)
+
+
 connection = SnowflakeConnConfig(accountname='jg84276.us-central1.gcp', warehousename="XSMALL_WH").create_connection()
-Schema = "KIV.ACCOUNT_USAGE"
-cqlib = CostProfile(connection, Schema)
-
-
-sdate = '2022-10-11'
-edate = '2022-10-21'
+cqlib = CostProfile(connection, st.session_state.Schema)
 
 st.set_page_config(
     page_title="Snowflake",
@@ -29,25 +24,26 @@ st.set_page_config(
     initial_sidebar_state="collapsed"
 )
 
-df = cqlib.total_cost_breakdown_ts(sdate, edate)
+# df = cqlib.total_cost_breakdown_ts(sdate, edate)
 # session_argument = st.session_state
 session_argument = {}
 
 if 'total_cost_df' not in st.session_state:
+    print("dates:  ", st.session_state.sdate, st.session_state.edate)
     st.session_state.total_cost_df = 0
-    st.session_state.total_cost_df = cqlib.total_cost_breakdown_ts(sdate, edate)
+    st.session_state.total_cost_df = cqlib.total_cost_breakdown_ts(st.session_state.sdate, st.session_state.edate)
 
 if 'cost_by_user_df' not in st.session_state:
     st.session_state.cost_by_user_df = 0
-    st.session_state.cost_by_user_df = cqlib.cost_by_user_ts(sdate, edate)
+    st.session_state.cost_by_user_df = cqlib.cost_by_user_ts(st.session_state.sdate, st.session_state.edate)
 
 if 'cost_by_wh_df' not in st.session_state:
     st.session_state.cost_by_wh_df = 0
-    st.session_state.cost_by_wh_df = cqlib.cost_by_wh_ts(sdate, edate)
+    st.session_state.cost_by_wh_df = cqlib.cost_by_wh_ts(st.session_state.sdate, st.session_state.edate)
 
 if 'cost_by_partner_tools_df' not in st.session_state:
     st.session_state.cost_by_partner_tools_df = 0
-    st.session_state.cost_by_partner_tools_df = cqlib.cost_by_partner_tool_ts(sdate, edate)
+    st.session_state.cost_by_partner_tools_df = cqlib.cost_by_partner_tool_ts(st.session_state.sdate, st.session_state.edate)
 
 with st.sidebar:
     st.header("Welcome to OPTIM")
