9df1d658677d8c6517f9f2bdac8a7bd4526ed281
giri-ggr <nawab.hayat@h1insights.com>
2023-02-28 21:49:20 +0530

    login changes added

--- webapp/config.py
+++ webapp/config.py
@@ -1,10 +1,13 @@
 import streamlit as st
+import datetime
+from datetime import timedelta
 from optiml.backend.cost_profile import get_previous_dates
 
 params = dict(
     Schema="KIV.ACCOUNT_USAGE",
-    sdate='2022-10-05',
-    edate='2022-10-12'
+    sdate=datetime.datetime.strptime('2022-10-05', "%Y-%m-%d").date(),
+    edate=datetime.datetime.strptime('2022-10-12', "%Y-%m-%d").date(),
+    TRAINING_LENGTH=30,
 )
 
 
@@ -14,3 +17,20 @@ def set_params():
         st.session_state[k] = v
     st.session_state.p1_sdate, st.session_state.p1_edate = get_previous_dates(st.session_state.sdate,
                                                                               st.session_state.edate, 1)
+    st.session_state['training_start'] = st.session_state["sdate"] - timedelta(st.session_state['TRAINING_LENGTH'] + 1)
+    st.session_state['training_end'] = st.session_state["sdate"] - timedelta(1)
+
+
+def authenticate_user(username, password):
+    with open('user.txt') as fp:
+        data = fp.read()
+    saved_user = data.split()[0].lower()
+    saved_password = data.split()[1]
+    print("saved: ",saved_password,saved_password)
+    if saved_user == username.lower() and saved_password == password:
+        return True
+    return False
+
+
+def change_state(key, value):
+    st.session_state[key] = value
