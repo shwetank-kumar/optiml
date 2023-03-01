dedb6aedb2224316be3f03cfd7c9b948ac0b4113
Shwetank Kumar <shwetank.kumar@gmail.com>
2023-03-01 08:17:44 -0800

    query analysis

--- optiml/backend/query_profile.py
+++ optiml/backend/query_profile.py
@@ -62,6 +62,97 @@ class QueryProfile(SNFLKQuery):
         df_unique.sort_values(metric,inplace=True,ascending=False)
         df_unique.reset_index()
         return df_unique
+    
+    def n_inefficient_queries_v2(self, start_date='', end_date='', n=10, metric='credits', unique=False):
+        """Inefficient queries as order by metric. Metric options:
+            bytes_scanned,
+            percentage_scanned_from_cache,
+            bytes_spilled_to_local_storage,
+            bytes_spilled_to_remote_storage,
+            percentage_partitions_scanned,
+            partitions_total,
+            total_time_elapsed_sec,
+            compilation_time_sec,
+            execution_time_sec,
+            queued_provisioning_time_sec,
+            queued_repair_time_sec,
+            queued_overload_time_sec,
+            list_external_files_time_sec,
+            total_time_elapsed_sec,
+            credits
+        """
+        
+        if not end_date:
+            today_date = date.today()
+            end_date = str(today_date)
+        
+        # credit_val = ''
+        # if self.credit_value:
+        #     credit_val = SNFLKQuery.credit_values[self.credit_value]
+
+        sql = f"""
+        WITH WAREHOUSE_SIZE AS
+        (
+            SELECT WAREHOUSE_SIZE, NODES
+            FROM (
+                    SELECT 'X-SMALL' AS WAREHOUSE_SIZE, 1 AS NODES
+                    UNION ALL
+                    SELECT 'SMALL' AS WAREHOUSE_SIZE, 2 AS NODES
+                    UNION ALL
+                    SELECT 'MEDIUM' AS WAREHOUSE_SIZE, 4 AS NODES
+                    UNION ALL
+                    SELECT 'LARGE' AS WAREHOUSE_SIZE, 8 AS NODES
+                    UNION ALL
+                    SELECT 'XLARGE' AS WAREHOUSE_SIZE, 16 AS NODES
+                    UNION ALL
+                    SELECT '2XLARGE' AS WAREHOUSE_SIZE, 32 AS NODES
+                    UNION ALL
+                    SELECT '3XLARGE' AS WAREHOUSE_SIZE, 64 AS NODES
+                    UNION ALL
+                    SELECT '4XLARGE' AS WAREHOUSE_SIZE, 128 AS NODES
+                    )
+        )
+            select qh.query_id,
+            qh.query_type,
+            HASH(qh.query_text) as query_hash,
+            qh.query_text,
+            qh.user_name,
+            qh.role_name,
+            qh.database_name,
+            qh.schema_name,
+            qh.warehouse_name,
+            qh.warehouse_size,
+            qh.warehouse_type,
+            qh.bytes_scanned,
+            round(qh.percentage_scanned_from_cache*100,2) as percentage_scanned_from_cache,
+            qh.bytes_spilled_to_local_storage,
+            qh.bytes_spilled_to_remote_storage,
+            round(qh.partitions_scanned/qh.partitions_total*100,2) as percentage_partitions_scanned,
+            qh.partitions_total,
+            qh.start_time,
+            qh.end_time,
+            round((qh.total_elapsed_time/(1000)),2) as total_time_elapsed_sec,
+            round((qh.compilation_time/(1000)),2) as compilation_time_sec,
+            round((qh.execution_time/1000),2) as execution_time_sec,
+            round((qh.queued_provisioning_time/1000),2) as queued_provisioning_time_sec,
+            round((qh.queued_repair_time/1000),2) as queued_repair_time_sec,
+            round((qh.queued_overload_time/1000),2) as queued_overload_time_sec,
+            round((qh.list_external_files_time/1000),2) as list_external_files_time_sec,
+            qh.query_tag,
+            qh.execution_status,
+            round((qh.execution_time/(1000*60*60))*ws.nodes,2) as credits
+
+        from {self.dbname}.QUERY_HISTORY QH
+        join WAREHOUSE_SIZE WS ON WS.WAREHOUSE_SIZE = upper(QH.WAREHOUSE_SIZE)
+        where date_trunc('day', qh.start_time) between '{start_date}' and '{end_date}'
+        order by {metric} desc
+        limit {n}   
+        """
+        df = self.query_to_df(sql)
+        df["n_success"] = (df["execution_status"] == "SUCCESS").astype(int)
+        df["n_fail"] = (df["execution_status"] == "FAIL").astype(int)
+        return df
+
 
     def n_inefficient_queries(self, start_date='', end_date='', n=10, metric='credits', unique=False):
         """Inefficient queries as order by metric. Metric options:
@@ -86,9 +177,9 @@ class QueryProfile(SNFLKQuery):
             today_date = date.today()
             end_date = str(today_date)
         
-        credit_val = ''
-        if self.credit_value:
-            credit_val = SNFLKQuery.credit_values[self.credit_value]
+        # credit_val = ''
+        # if self.credit_value:
+        #     credit_val = SNFLKQuery.credit_values[self.credit_value]
 
         sql = f"""
         WITH WAREHOUSE_SIZE AS
@@ -152,43 +243,69 @@ class QueryProfile(SNFLKQuery):
         df["n_fail"] = (df["execution_status"] == "FAIL").astype(int)
         return df
 
-
-    def queries_by_execution_status(self, start_date='', end_date=''):
+    def queries_stats_by_execution_status(self, start_date='', end_date=''):
         if not end_date:
             today_date = date.today()
             end_date = str(today_date)
         
-        credit_val = ''
-        if self.credit_value:
-            credit_val = SNFLKQuery.credit_values[self.credit_value]
+        # credit_val = ''
+        # if self.credit_value:
+        #     credit_val = SNFLKQuery.credit_values[self.credit_value]
 
         
         sql = f"""
+            WITH WAREHOUSE_SIZE AS
+            (
+                SELECT WAREHOUSE_SIZE, NODES
+                FROM (
+                        SELECT 'X-SMALL' AS WAREHOUSE_SIZE, 1 AS NODES
+                        UNION ALL
+                        SELECT 'SMALL' AS WAREHOUSE_SIZE, 2 AS NODES
+                        UNION ALL
+                        SELECT 'MEDIUM' AS WAREHOUSE_SIZE, 4 AS NODES
+                        UNION ALL
+                        SELECT 'LARGE' AS WAREHOUSE_SIZE, 8 AS NODES
+                        UNION ALL
+                        SELECT 'XLARGE' AS WAREHOUSE_SIZE, 16 AS NODES
+                        UNION ALL
+                        SELECT '2XLARGE' AS WAREHOUSE_SIZE, 32 AS NODES
+                        UNION ALL
+                        SELECT '3XLARGE' AS WAREHOUSE_SIZE, 64 AS NODES
+                        UNION ALL
+                        SELECT '4XLARGE' AS WAREHOUSE_SIZE, 128 AS NODES
+                        )
+            )
             select
                 qh.user_name, 
                 qh.execution_status, 
                 qh.warehouse_name,
+                qh.warehouse_size,
                 date_trunc('day', qh.start_time) as day,
+                ROUND(sum((EXECUTION_TIME/(1000*60*60))*WS.NODES),2) as CREDITS,
                 count(*) as counts
             from 
                 {self.dbname}.query_history qh
+            JOIN 
+                WAREHOUSE_SIZE WS ON WS.WAREHOUSE_SIZE = upper(QH.WAREHOUSE_SIZE)
             where 
                 date_trunc('day', qh.start_time) between '{start_date}' and '{end_date}'
+            and qh.warehouse_size is not NULL
             group by 
                 qh.user_name, 
                 qh.execution_status, 
                 qh.warehouse_name,
+                qh.warehouse_size,
                 day
             order by 
                 user_name, day;
         """
 
         df = self.query_to_df(sql)
-        df = df.fillna("Unassigned")
         df["n_success"] = (df["execution_status"] == "SUCCESS").astype(int)*df["counts"]
+        df["credits_success"] = (df["execution_status"] == "SUCCESS").astype(int)*df["credits"]
         df["n_fail"] = (df["execution_status"] == "FAIL").astype(int)*df["counts"]
+        df["credits_fail"] = (df["execution_status"] == "FAIL").astype(int)*df["credits"]
         return df
-
 
     
     def n_most_executed_select_queries(self, start_date='2022-01-01',end_date='', n=10):
@@ -266,13 +383,10 @@ class QueryProfile(SNFLKQuery):
         df=self.query_to_df(sql)
         return df
 
-    def queries_by_wh(self,start_datetime="2022-01-01", end_datetime="", warehouse_name='DEV_WH'):
+    def queries_by_execution_status(self,start_datetime="2022-01-01", end_datetime="", execution_status='FAIL'):
         """
         Shows queries issued in specific warehouse in given time period.
         """
-        # if not end_date:
-        #     today_date = date.today()
-        #     end_date = str(today_date)
         sql=f"""
         WITH WAREHOUSE_SIZE AS
         (
@@ -324,7 +438,7 @@ class QueryProfile(SNFLKQuery):
         FROM {self.dbname}.QUERY_HISTORY QH
         JOIN WAREHOUSE_SIZE WS ON WS.WAREHOUSE_SIZE = upper(QH.WAREHOUSE_SIZE)
         where START_TIME between '{start_datetime}' and '{end_datetime}'
-        and warehouse_name='{warehouse_name}'
+        and execution_status='{execution_status}'
         order by credits desc
         """
         df=self.query_to_df(sql)
@@ -339,149 +453,53 @@ class QueryProfile(SNFLKQuery):
         df = self.query_to_df(sql)
         return df
 
-    # def get_queries_by_wh(self,\
-    #                 start_datetime=None,\
-    #                 end_datetime=None,\
-    #                 warehouse_name=None):
-    #     """Gets queries running at a particular time, by a specific user, 
-    #     in a specific warehouse """
+
+    # def n_inefficient_queries_v2(self, start_date='', end_date='', n=10, metric='credits', unique=False):        
+    #     if not end_date:
+    #         today_date = date.today()
+    #         end_date = str(today_date)
         
-    #     # start_datetime = start_datetime or "start_datetime"
-    #     # end_datetime = end_datetime or "end_datetime"
-    #     # user_name = user_name or "user_name"
-    #     # warehouse_name = warehouse_name or 'warehouse_name'
+    #     # credit_val = ''
+    #     # if self.credit_value:
+    #     #     credit_val = SNFLKQuery.credit_values[self.credit_value]
 
     #     sql = f"""
-    #         select qh.query_id,
-    #         qh.query_type,
+    #     WITH WAREHOUSE_SIZE AS
+    #     (
+    #         SELECT WAREHOUSE_SIZE, NODES
+    #         FROM (
+    #                 SELECT 'X-SMALL' AS WAREHOUSE_SIZE, 1 AS NODES
+    #                 UNION ALL
+    #                 SELECT 'SMALL' AS WAREHOUSE_SIZE, 2 AS NODES
+    #                 UNION ALL
+    #                 SELECT 'MEDIUM' AS WAREHOUSE_SIZE, 4 AS NODES
+    #                 UNION ALL
+    #                 SELECT 'LARGE' AS WAREHOUSE_SIZE, 8 AS NODES
+    #                 UNION ALL
+    #                 SELECT 'XLARGE' AS WAREHOUSE_SIZE, 16 AS NODES
+    #                 UNION ALL
+    #                 SELECT '2XLARGE' AS WAREHOUSE_SIZE, 32 AS NODES
+    #                 UNION ALL
+    #                 SELECT '3XLARGE' AS WAREHOUSE_SIZE, 64 AS NODES
+    #                 UNION ALL
+    #                 SELECT '4XLARGE' AS WAREHOUSE_SIZE, 128 AS NODES
+    #                 )
+    #     )
+    #         select 
+    #         HASH(qh.query_text) as query_hash,
     #         qh.query_text,
-    #         qh.user_name,
-    #         qh.role_name,
-    #         qh.database_name,
-    #         qh.schema_name,
     #         qh.warehouse_name,
-    #         qh.warehouse_size,
-    #         qh.warehouse_type,
-    #         qh.bytes_scanned,
-    #         round(qh.percentage_scanned_from_cache*100,2) as percentage_scanned_from_cache,
-    #         qh.bytes_spilled_to_local_storage,
-    #         qh.bytes_spilled_to_remote_storage,
-    #         qh.partitions_total,
-    #         qh.start_time,
-    #         qh.end_time,
-    #         qh.query_tag,
-    #         qh.execution_status,
-    #         round((qh.total_elapsed_time/(1000)),2) as total_time_elapsed_sec,
-    #         round((qh.compilation_time/(1000)),2) as compilation_time_sec,
-    #         round((qh.execution_time/1000),2) as execution_time_sec,
-    #         round((qh.queued_provisioning_time/1000),2) as queued_provisioning_time_sec,
-    #         round((qh.queued_repair_time/1000),2) as queued_repair_time_sec,
-    #         round((qh.queued_overload_time/1000),2) as queued_overload_time_sec,
-    #         round((qh.list_external_files_time/1000),2) as list_external_files_time_sec
-    #             from {self.dbname}.query_history qh
-    #             where qh.start_time between '{start_datetime}' and '{end_datetime}'
-    #             and qh.warehouse_name='{warehouse_name}'
-    #         """
-        
+    #         qh.user_name,
+    #         sum(round((qh.execution_time/(1000*60*60))*ws.nodes,2)) as credits
+    #     from {self.dbname}.QUERY_HISTORY qh
+    #     join WAREHOUSE_SIZE WS ON WS.WAREHOUSE_SIZE = upper(QH.WAREHOUSE_SIZE)
+    #     where date_trunc('day', qh.start_time) between '{start_date}' and '{end_date}'
+    #     group by 1,2,3,4
+    #     order by {metric} desc
+    #     limit {n}   
+    #     """
     #     df = self.query_to_df(sql)
     #     return df
-
-
-    # def get_queries_by_wh_with_credits(self,\
-    #                 start_datetime=None,\
-    #                 end_datetime=None,\
-    #                 warehouse_name=None):
-    #     """Gets queries running at a particular time, by a specific user, 
-    #     in a specific warehouse """
-        
-    #     # start_datetime = start_datetime or "start_datetime"
-    #     # end_datetime = end_datetime or "end_datetime"
-    #     # user_name = user_name or "user_name"
-    #     # warehouse_name = warehouse_name or 'warehouse_name'
-
-    #     sql = f"""
-    #         select qh.query_id,
-    #         qh.query_type,
-    #         qh.query_text,
-    #         qh.user_name,
-    #         qh.role_name,
-    #         qh.database_name,
-    #         qh.schema_name,
-    #         qh.warehouse_name,
-    #         qh.warehouse_size,
-    #         qh.warehouse_type,
-    #         qh.bytes_scanned,
-    #         round(qh.percentage_scanned_from_cache*100,2) as percentage_scanned_from_cache,
-    #         qh.bytes_spilled_to_local_storage,
-    #         qh.bytes_spilled_to_remote_storage,
-    #         qh.partitions_total,
-    #         qh.start_time,
-    #         qh.end_time,
-    #         qh.query_tag,
-    #         qh.execution_status,
-    #         round((qh.total_elapsed_time/(1000)),2) as total_time_elapsed_sec,
-    #         round((qh.compilation_time/(1000)),2) as compilation_time_sec,
-    #         round((qh.execution_time/1000),2) as execution_time_sec,
-    #         round((qh.queued_provisioning_time/1000),2) as queued_provisioning_time_sec,
-    #         round((qh.queued_repair_time/1000),2) as queued_repair_time_sec,
-    #         round((qh.queued_overload_time/1000),2) as queued_overload_time_sec,
-    #         round((qh.list_external_files_time/1000),2) as list_external_files_time_sec
-    #             from {self.dbname}.query_history qh
-    #             where qh.start_time between '{start_datetime}' and '{end_datetime}'
-    #             and qh.warehouse_name='{warehouse_name}'
-    #         """
-        
-    #     df = self.query_to_df(sql)
-    #     return df
-    
-
-
-    def n_inefficient_queries_v2(self, start_date='', end_date='', n=10, metric='credits', unique=False):        
-        if not end_date:
-            today_date = date.today()
-            end_date = str(today_date)
-        
-        credit_val = ''
-        if self.credit_value:
-            credit_val = SNFLKQuery.credit_values[self.credit_value]
-
-        sql = f"""
-        WITH WAREHOUSE_SIZE AS
-        (
-            SELECT WAREHOUSE_SIZE, NODES
-            FROM (
-                    SELECT 'X-SMALL' AS WAREHOUSE_SIZE, 1 AS NODES
-                    UNION ALL
-                    SELECT 'SMALL' AS WAREHOUSE_SIZE, 2 AS NODES
-                    UNION ALL
-                    SELECT 'MEDIUM' AS WAREHOUSE_SIZE, 4 AS NODES
-                    UNION ALL
-                    SELECT 'LARGE' AS WAREHOUSE_SIZE, 8 AS NODES
-                    UNION ALL
-                    SELECT 'XLARGE' AS WAREHOUSE_SIZE, 16 AS NODES
-                    UNION ALL
-                    SELECT '2XLARGE' AS WAREHOUSE_SIZE, 32 AS NODES
-                    UNION ALL
-                    SELECT '3XLARGE' AS WAREHOUSE_SIZE, 64 AS NODES
-                    UNION ALL
-                    SELECT '4XLARGE' AS WAREHOUSE_SIZE, 128 AS NODES
-                    )
-        )
-            select 
-            HASH(qh.query_text) as query_hash,
-            qh.query_text,
-            qh.warehouse_name,
-            qh.user_name,
-            sum(round((qh.execution_time/(1000*60*60))*ws.nodes,2)) as credits
-        from {self.dbname}.QUERY_HISTORY qh
-        join WAREHOUSE_SIZE WS ON WS.WAREHOUSE_SIZE = upper(QH.WAREHOUSE_SIZE)
-        where date_trunc('day', qh.start_time) between '{start_date}' and '{end_date}'
-        group by 1,2,3,4
-        order by {metric} desc
-        limit {n}   
-        """
-        df = self.query_to_df(sql)
-        return df
 
 
     def n_most_executed_select_queries_v2(self, start_date='',end_date='', n=10):
